1714a38d3b40b9974f4337e04b6354f590c148d6
bojan55 <bojan.bajic55@gmail.com>
2025-08-16 12:15:51 +0200

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int min = 1, max = 49;
+        int slots = 7;
+        int [] lottoArray = new int[slots];
+        boolean isRepeated;
+        int randomNum = 0;
+
+
+    }
+}
